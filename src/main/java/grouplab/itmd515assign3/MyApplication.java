c035e74d7b4866418c0492c88c69828589d1a1bf
geekbitcreations <dbarndt@hawk.iit.edu>
2018-03-08 09:30:29 -0600

    Initial commit

---
+++ src/main/java/grouplab/itmd515assign3/MyApplication.java
@@ -0,0 +1,15 @@
+package grouplab.itmd515assign3;
+
+/*
+	Deborah Barndt
+	Thomas Boller
+	3-8-18
+	MyApplication.java
+	Programming Assignment 3
+	This is program holds the MyApplication class.
+*/
+
+public class MyApplication 
+{
+	
+}
