760c771bbfa57cc9fc38abfa3ea70247f9e6770f
geekbitcreations <dbarndt@hawk.iit.edu>
2018-03-08 10:44:00 -0600

    adding test class

---
+++ src/main/java/grouplab/itmd515assign3/MyMessageDITest.java
@@ -0,0 +1,31 @@
+package grouplab.itmd515assign3;
+
+/*
+	Deborah Barndt
+	3-8-18
+	MyMessageDITest.java
+	Programming Assignment 3
+	This is program holds the MyMessageDITest class.
+*/
+
+public class MyMessageDITest 
+{
+	public static void main(String[] args)
+	{
+		String msg = "Hi Deborah";
+		String email = "dbarndt@hawk.iit.edu";
+		String phone = "8888675309";
+		MessageServiceInjector injector = null;
+		Consumer app = null;
+		
+		// Send email
+		injector = new EmailServiceInjector();
+		app = injector.getConsumer();
+		app.processMessages(msg, email);
+		
+		// Send SMS
+		injector = new SMSServiceInjector();
+		app = injector.getConsumer();
+		app.processMessages(msg, phone);
+	}
+}
